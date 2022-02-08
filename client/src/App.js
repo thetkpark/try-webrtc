e387fb1b688c40ae6f1cdd0a2610624da134a8bc
Sethanant Pipatpakorn <the_tkpark@hotmail.com>
2022-02-08 22:41:54 +0700

    add setting username

--- client/src/App.js
+++ client/src/App.js
@@ -4,6 +4,8 @@ import io from 'socket.io-client'
 function App() {
   const [localStream, setLocalStream] = useState(null)
   const [remoteStream, setRemoteStream] = useState(null)
+  const [username, setUsername] = useState('')
+  const [onlineUsers, setOnlineUsers] = useState([])
 
   const socket = useRef()
   const localVideo = useRef()
@@ -11,15 +13,50 @@ function App() {
 
   useEffect(() => {
     socket.current = io('http://localhost:8080', { transports: ['websocket'] })
+    socket.current.on('online-users', ({ users }) => {
+      console.log(users)
+      setOnlineUsers(users)
+    })
+
     navigator.mediaDevices.getUserMedia({ audio: true, video: true }).then(stream => {
       setLocalStream(stream)
       localVideo.current.srcObject = stream
     })
   }, [])
 
+  const onSubmitUsername = e => {
+    e.preventDefault()
+    socket.current.emit('set-username', { username })
+  }
+
+  const onCallUser = id => {
+    console.log(`Calling ${id}`)
+  }
+
+  const renderOnlineUsers = () => {
+    return (
+      <div>
+        <ul>
+          {onlineUsers.map(user => (
+            <span>
+              <li key={user.id}>{user.username}</li>
+              <button onClick={() => onCallUser(user.id)}>Call</button>
+            </span>
+          ))}
+        </ul>
+      </div>
+    )
+  }
+
   return (
     <div className="App">
       <video playsInline autoPlay ref={localVideo}></video>
+      <form onSubmit={onSubmitUsername}>
+        <label>Username</label>
+        <input type="text" value={username} onChange={e => setUsername(e.target.value)} />
+        <button type="submit">Submit</button>
+      </form>
+      {renderOnlineUsers()}
     </div>
   )
 }
--- server/src/index.js
+++ server/src/index.js
@@ -4,11 +4,13 @@ const app = express()
 
 const server = http.createServer(app)
 const io = require("socket.io")(server, {
-	cors: {
-		origin: "http://localhost:3000",
-		credentials: true
-	}
+	// cors: {
+	// 	origin: "http://localhost:3000",
+	// 	credentials: true
+	// }
 })
+
+const users = {}
 
 server.listen(8080)
 
@@ -16,6 +18,19 @@ app.get("/api/ping", (req, res) => {
 	res.send({ success: true, timestamp: Date.now() })
 })
 
-io.on("connection", () => {
+io.on("connection", (socket) => {
 	console.log("A user is connected")
+	if (!users[socket.id]) {
+		users[socket.id] = { id: socket.id }
+	}
+
+	socket.on("set-username", ({ username }) => {
+		users[socket.id] = { ...users[socket.id], username }
+		console.log(users)
+		broadcastOnlineUsers(socket.id)
+	})
 })
+
+const broadcastOnlineUsers = (socketId) => {
+	io.sockets.emit("online-users", { users: Object.values(users) })
+}
